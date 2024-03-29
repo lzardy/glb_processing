c75b14b1d5237041d62c56a0c14ad173ca5528e0
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-03-29 02:23:17 +0000

    Update glb_to_ply.py

--- glb_to_ply.py
+++ glb_to_ply.py
@@ -1,31 +1,59 @@
+# takes a folder of GLB files and processes them into 
 import bpy
 import glob
 import os
+from os import mkdir
+from os.path import isdir
 import pathlib
-importDir = "/home/r/Desktop/GLB/"
-outputDir = "/home/r/Desktop/PLY/"
-print(importDir)
-os.chdir(importDir)
 
-counter = 0
-for file in glob.glob("*.glb"):
+# ---
+
+importDir = "GLB/"
+outputDir = "PLY0/"
+if not isdir(outputDir): mkdir(outputDir)
+
+for file in glob.glob(importDir + "*.glb"):
     model_name = pathlib.Path(file).stem
     if pathlib.Path(outputDir+model_name+'.ply').is_file() == True: continue
-    #print("Model: " + model_name)
-    bpy.ops.import_scene.gltf(filepath=file)
-    if bpy.context.active_object.type != 'MESH': 
-        bpy.ops.object.delete(use_global=False)
-        bpy.ops.outliner.orphans_purge()
-        bpy.ops.outliner.orphans_purge()
-        bpy.ops.outliner.orphans_purge()
-    for obj in bpy.context.selected_objects: obj.name = model_name
+    try:
+        bpy.ops.import_scene.gltf(filepath=file)
+    except:
+        continue
+    bpy.ops.wm.ply_export(
+                            filepath=outputDir+model_name+'.ply',
+                            filter_glob='*.ply',
+                            check_existing=False,
+                            ascii_format=False,
+                            export_selected_objects=True,
+                            apply_modifiers=True,
+                            export_triangulated_mesh=True,
+                            export_normals=True,
+                            export_uv=False,
+                            export_colors='NONE',
+                            global_scale=1.0,
+                            forward_axis='Y',
+                            up_axis='Z'
+                        )
+    bpy.ops.object.select_all(action='SELECT')
+    bpy.ops.object.delete(use_global=False)
+    bpy.ops.outliner.orphans_purge()
+    bpy.ops.outliner.orphans_purge()
+    bpy.ops.outliner.orphans_purge()
+
+# ---
+
+importDir = "PLY0/"
+outputDir = "PLY1/"
+if not isdir(outputDir): mkdir(outputDir)
+
+counter = 0
+for file in glob.glob(importDir + "*.ply"):
+    model_name = pathlib.Path(file).stem
+    if pathlib.Path(outputDir+model_name+'.ply').is_file() == True: continue
+    bpy.ops.wm.ply_import(filepath=file)
     counter = counter + 1
     if counter > 10:
         bpy.ops.object.select_all(action='SELECT')
-        bpy.ops.object.mode_set(mode='EDIT')
-        bpy.ops.mesh.select_all(action='SELECT')
-        bpy.ops.mesh.remove_doubles(threshold=0.001)
-        bpy.ops.object.mode_set(mode='OBJECT')
         
         mesh_obs = (o for o in bpy.context.selected_objects
         if o.type == 'MESH' and o.dimensions.length)
@@ -36,15 +64,12 @@ for file in glob.glob("*.glb"):
         bpy.ops.object.origin_set(type='GEOMETRY_ORIGIN', center='BOUNDS')
         
         for obj in bpy.context.selected_objects: obj.location = (0,0,0)
-    
-        for obj in bpy.context.selected_objects:
-            obj.data.color_attributes.new("Color", 'FLOAT_COLOR', 'CORNER')
-        bpy.ops.object.bake(type='DIFFUSE')
+
         for obj in bpy.context.selected_objects:
             bpy.ops.object.select_all(action='DESELECT')
             obj.select_set(True)
             bpy.ops.wm.ply_export(
-                                    filepath=outputDir+obj.name+'.ply',
+                                    filepath=outputDir+model_name+'.ply',
                                     filter_glob='*.ply',
                                     check_existing=False,
                                     ascii_format=False,
@@ -53,11 +78,12 @@ for file in glob.glob("*.glb"):
                                     export_triangulated_mesh=True,
                                     export_normals=True,
                                     export_uv=False,
-                                    export_colors='SRGB',
+                                    export_colors='NONE',
                                     global_scale=1.0,
                                     forward_axis='Y',
                                     up_axis='Z'
                                 )
+                                
         bpy.ops.object.select_all(action='SELECT')
         bpy.ops.object.delete(use_global=False)
         bpy.ops.outliner.orphans_purge()
@@ -71,17 +97,12 @@ bpy.ops.outliner.orphans_purge()
 bpy.ops.outliner.orphans_purge()
 bpy.ops.outliner.orphans_purge()
 
-for file in glob.glob("*.glb"):
+for file in glob.glob(importDir + "*.ply"):
     model_name = pathlib.Path(file).stem
     if pathlib.Path(outputDir+model_name+'.ply').is_file() == True: continue
-    #print("Model: " + model_name)
-    bpy.ops.import_scene.gltf(filepath=file)
-    for obj in bpy.context.selected_objects: obj.name = model_name
+    bpy.ops.wm.ply_import(filepath=file)
+    
     bpy.ops.object.select_all(action='SELECT')
-    bpy.ops.object.mode_set(mode='EDIT')
-    bpy.ops.mesh.select_all(action='SELECT')
-    bpy.ops.mesh.remove_doubles(threshold=0.001)
-    bpy.ops.object.mode_set(mode='OBJECT')
     
     mesh_obs = (o for o in bpy.context.selected_objects
     if o.type == 'MESH' and o.dimensions.length)
@@ -94,13 +115,10 @@ for file in glob.glob("*.glb"):
     for obj in bpy.context.selected_objects: obj.location = (0,0,0)
     
     for obj in bpy.context.selected_objects:
-        obj.data.color_attributes.new("Color", 'FLOAT_COLOR', 'CORNER')
-    bpy.ops.object.bake(type='DIFFUSE')
-    for obj in bpy.context.selected_objects:
         bpy.ops.object.select_all(action='DESELECT')
         obj.select_set(True)
         bpy.ops.wm.ply_export(
-                                filepath=outputDir+obj.name+'.ply',
+                                filepath=outputDir+model_name+'.ply',
                                 filter_glob='*.ply',
                                 check_existing=False,
                                 ascii_format=False,
@@ -109,7 +127,7 @@ for file in glob.glob("*.glb"):
                                 export_triangulated_mesh=True,
                                 export_normals=True,
                                 export_uv=False,
-                                export_colors='SRGB',
+                                export_colors='NONE',
                                 global_scale=1.0,
                                 forward_axis='Y',
                                 up_axis='Z'
@@ -119,5 +137,3 @@ for file in glob.glob("*.glb"):
     bpy.ops.outliner.orphans_purge()
     bpy.ops.outliner.orphans_purge()
     bpy.ops.outliner.orphans_purge()
-
-    
