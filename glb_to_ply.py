6dad26a79f9dee91354db8ef8498130208765fc5
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-03-29 02:31:26 +0000

    Update glb_to_ply.py

--- glb_to_ply.py
+++ glb_to_ply.py
@@ -1,4 +1,9 @@
 # takes a folder of GLB files and processes them into PLY files
+# strips textures and vertex colors leaving only vertex pos, vertex norm, index buffer
+# 1. merges model into one contiguous mesh
+# 2. scales the model to a unit sphere
+# 3. centers the origin
+# 4. centers the model
 import bpy
 import glob
 import os
