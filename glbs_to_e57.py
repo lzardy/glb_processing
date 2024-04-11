2af471fcd063aea1c5f84a2808576f7e02589f7e
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-04-11 21:57:52 +0100

    Create glbs_to_e57.py

---
+++ glbs_to_e57.py
@@ -0,0 +1,14 @@
+# this is more useful as meshlab will export textures to e57 pointclouds (not vertex colors though)
+# all we want to train our networks on is point clouds, as all we want them to produce are pointclouds
+from pathlib import Path
+import os
+import pymeshlab as ml
+meshlab_server = ml.MeshSet()
+root_dir = Path('in')
+for file_path in root_dir.glob('**/*'):
+    if file_path.is_file():
+        try:
+            meshlab_server.load_new_mesh(str(file_path))
+            meshlab_server.save_current_mesh('out/' + os.path.splitext(os.path.basename(file_path))[0] + '.e57')
+        except Exception:
+            pass
