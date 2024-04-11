1a7ebc241a68fd73576f6537dd625bfeefb27efa
James William Fletcher <78346668+mrbid@users.noreply.github.com>
2024-04-11 21:58:42 +0100

    Update glbs_to_e57.py

--- glbs_to_e57.py
+++ glbs_to_e57.py
@@ -4,11 +4,11 @@ from pathlib import Path
 import os
 import pymeshlab as ml
 meshlab_server = ml.MeshSet()
-root_dir = Path('in')
+root_dir = Path('/media/hpe/SG-20TB/objaverse/glbs')
 for file_path in root_dir.glob('**/*'):
     if file_path.is_file():
         try:
             meshlab_server.load_new_mesh(str(file_path))
-            meshlab_server.save_current_mesh('out/' + os.path.splitext(os.path.basename(file_path))[0] + '.e57')
+            meshlab_server.save_current_mesh('/media/hpe/SG-20TB/plyverse/' + os.path.splitext(os.path.basename(file_path))[0] + '.e57')
         except Exception:
             pass
